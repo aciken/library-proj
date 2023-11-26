1ae5e0d8009c380789e8d70f380c0f32b61cdfce
aciken <adrianmarton2006@gmail.com>
2023-11-26 13:12:27 +0100

    started project

---
+++ main.js
@@ -0,0 +1,14 @@
+const 
+
+
+const myLibrary = [];
+
+function Book(Name, writer,relese){
+    this.Name = Name;
+    this.writer = writer;
+    this.relese = relese;
+    this.read = false;
+}
+
+
+
